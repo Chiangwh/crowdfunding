b2ad2851560c65fd32b202c25953a6e97f55d22a
Chiang Wen Hsin <chiang@anyroad.com>
2020-10-26 23:31:43 +0800

    second commit

--- project/crowdfunding/urls.py
+++ project/crowdfunding/urls.py
@@ -2,6 +2,8 @@ from django.urls import path,re_path
 from . import views # Import the views
 
 urlpatterns = [
-    path('',views.login),
-    re_path(r'register',views.register),
+    path('',views.home, name = "home"),
+    re_path(r'^register',views.register, name ="register"),
+    re_path(r'^login',views.loginpage, name ="loginpage"),
+    re_path(r'^logout',views.logoutUser, name ="logout"),
 ]
--- project/crowdfunding/views.py
+++ project/crowdfunding/views.py
@@ -1,8 +1,51 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect 
+from django.http import HttpResponse
+from django.forms import inlineformset_factory
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User,auth
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib import messages 
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-def login(request): #Renders 'crowdfunding/login.html'template
-    return render(request,'crowdfunding/login.html')
+from .forms import CreateUserForm
+
+@login_required(login_url='loginpage') #send user back to login page if not login
+def home(request): #Renders 'crowdfunding/home.html'template 
+    return render(request,'crowdfunding/home.html')
+
+def loginpage(request): #Renders 'crowdfunding/login.html'template 
+    
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username= username, password=password)
+
+        if user is not None:
+             login(request, user)
+             return redirect('home')
+        else:
+            messages.info(request,'Username OR Password is incorrect')
+            
+
+    context = {}
+    return render(request,'crowdfunding/login.html',context)
+
+def logoutUser(request):  #logout view, sending user back to login page
+    logout(request)
+    return redirect('loginpage')
 
 def register(request): #Renders 'crowdfunding/register.html'template
-    return render(request,'crowdfunding/register.html')
+    form = CreateUserForm()
+
+    if request.method =='POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request,'Account was created for '+ user)
+            return redirect ('crowdfunding/login.html')
+
+    context = {'form':form}
+    return render(request,'crowdfunding/register.html',context)
